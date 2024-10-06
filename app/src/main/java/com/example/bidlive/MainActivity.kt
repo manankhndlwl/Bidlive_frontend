baaba5ec1c8064c8ee56ad1dc4b0386e884fe7fd
manankhndlwl <162955550+manankhndlwl@users.noreply.github.com>
2024-10-06 16:29:18 +0530

    initial commit

---
+++ app/src/main/java/com/example/bidlive/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.bidlive
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bidlive.ui.theme.BidLiveTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BidLiveTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val Image = painterResource(id = R.drawable.bro)
+    Row {
+        Image(painter = Image, contentDescription = "auction_gallery",contentScale = ContentScale.Crop,
+            modifier = Modifier.padding(10.dp))
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BidLiveTheme {
+        Greeting("Android")
+    }
+}
